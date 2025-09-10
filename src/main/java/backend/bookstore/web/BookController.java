8526bfb42c7d5ac5b048c7319acd67999def5572
Jenny <205511984+jenny492@users.noreply.github.com>
2025-09-10 18:45:34 +0300

    Siistitty koodia, aloitettu editin teko

--- src/main/java/backend/bookstore/web/BookController.java
+++ src/main/java/backend/bookstore/web/BookController.java
@@ -1,6 +1,5 @@
 package backend.bookstore.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
@@ -16,28 +15,20 @@ import backend.bookstore.domain.BookRepository;
 @Controller
 public class BookController {
 
+    //@Autowired
+    private BookRepository bookRepository;
+
+    // construktorin injektointi
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @GetMapping("/index")
     public @ResponseBody String indexMethod() {
         return "Bookstore";
     }
 
-    @Bean
-    public CommandLineRunner demo(BookRepository bookRepository) {
-        return (args) -> {
-            // luodaan muutama kirja
-            Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565", 10.99, "1925");
-            Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", "9780061120084", 7.99, "1960");
-            Book book3 = new Book("1984", "George Orwell", "9780451524935", 9.99, "1949");
-            // tallennetaan kirjat h2-tietokantaan
-            bookRepository.save(book1);
-            bookRepository.save(book2);
-            bookRepository.save(book3);
-        };
-    }
-
-    @Autowired
-    private BookRepository bookRepository;
-
+    // tuo kaikki kirjat
     @GetMapping("/booklist")
     public String bookList(Model model) {
         model.addAttribute("books", bookRepository.findAll());
@@ -62,4 +53,23 @@ public class BookController {
         return "redirect:../booklist";
     }
 
+    @GetMapping("path")
+    public String edit(Model model) {
+        model.addAttribute("book", model);
+        return new String();
+    }
+
+    @Bean
+    public CommandLineRunner demo(BookRepository bookRepository) {
+        return (args) -> {
+            // luodaan muutama kirja
+            Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565", 10.99, "1925");
+            Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", "9780061120084", 7.99, "1960");
+            Book book3 = new Book("1984", "George Orwell", "9780451524935", 9.99, "1949");
+            // tallennetaan kirjat h2-tietokantaan
+            bookRepository.save(book1);
+            bookRepository.save(book2);
+            bookRepository.save(book3);
+        };
+    }
 }
