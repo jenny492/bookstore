ebe68281117adbc83d0030a4614085089888eae5
Jenny <205511984+jenny492@users.noreply.github.com>
2025-09-20 14:50:03 +0300

    Lisätty bookrestcontrolleriin edit ja delete

--- src/main/java/backend/bookstore/web/BookRestController.java
+++ src/main/java/backend/bookstore/web/BookRestController.java
@@ -7,11 +7,16 @@ import backend.bookstore.domain.CategoryRepository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
+//Rest palauttaa tietoa json-muodossa tekstinä
 public class BookRestController {
 
     private final BookRepository bookRepository;
@@ -32,5 +37,29 @@ public class BookRestController {
         return bookRepository.findById(bookId);
     }
     
-    
+    @PostMapping("/books")
+    public Book saveBook(@RequestBody Book book) {     
+        return bookRepository.save(book);
+    }
+
+    @PostMapping("/books/{id}")
+    public Book editBook(@PathVariable("id") Long bookId, @RequestBody Book editedBook) { 
+        return bookRepository.findById(bookId)
+        .map(book -> {
+            book.setAuthor(editedBook.getAuthor());
+            book.setCategory(editedBook.getCategory());
+            book.setIsbn(editedBook.getIsbn());
+            book.setPrice(editedBook.getPrice());
+            book.setPublicationYear(editedBook.getPublicationYear());
+            book.setTitle(editedBook.getTitle());
+            return bookRepository.save(book);
+        })  
+        .orElse(null);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteBook(@PathVariable Long bookId) {
+        bookRepository.deleteById(bookId);
+        //tee joku tarkistus
+    }
 }
